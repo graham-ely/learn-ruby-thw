ca598d9119e04f205f30e06c1ce6286ddf1ae6c4
graham-ely <graham.ely@gatech.edu>
2018-02-25 03:43:46 -0500

    Exercises 13 & 14

--- exer_11-20/ex11.rb
+++ exer_11-20/ex11.rb
@@ -1,8 +1,8 @@
 print "How old are you? "
-age = gets.chomp
+age = $stdin.gets.chomp
 print "How tall are you? "
-height = gets.chomp
+height = $stdin.gets.chomp
 print "How much do you weigh? "
-weight = gets.chomp
+weight = $stdin.gets.chomp
 
 puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
